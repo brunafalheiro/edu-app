41b51584f9953faed32921f0986b91fb4d5f37d1
brunafalheiro <brunafalheiro@yahoo.com.br>
2025-02-20 18:43:57 -0300

    Init menu card

---
+++ src/components/MenuCard/MenuCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="menu-card flex space-between pointer pt-24 pr-24 pb-24 pl-24">
+    <p class="title">{{title}}</p>
+    <div class="icon"></div>
+  </div>
+</template>
+
+<script setup>
+  import { defineProps } from 'vue'
+  defineProps({ title: String });
+</script>
+
+<style lang="scss">@import 'MenuCard.scss';</style>
