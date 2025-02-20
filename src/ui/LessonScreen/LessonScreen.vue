2fd61332edc44dbd781729d0a22272ad87f323f1
brunafalheiro <brunafalheiro@yahoo.com.br>
2025-02-20 18:44:10 -0300

    Init lesson screen

---
+++ src/ui/LessonScreen/LessonScreen.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Lesson {{routeId}}</div>
+</template>
+
+<script setup>
+  import { useRoute } from 'vue-router';
+
+  const route = useRoute();
+  const routeId = route.params.id;
+</script>
+
+<style lang="scss">@import 'LessonScreen.scss';</style>
