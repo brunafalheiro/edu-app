85fb6a6d0be6481573eaebdb933dd61620f1ec4b
brunafalheiro <brunafalheiro@yahoo.com.br>
2025-02-20 18:44:30 -0300

    Init edu button

---
+++ src/components/EduButton/EduButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button class="button full-width pointer" @click="onclick">{{title}}</button>
+</template>
+
+<script setup>
+  import { defineProps } from 'vue'
+  
+  defineProps({
+    title: String,
+    onclick: Function,
+  });
+</script>
+
+<style lang="scss">@import 'EduButton.scss';</style>
