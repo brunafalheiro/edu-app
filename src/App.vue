c7c4170ed979a218427b53f6d9eeb14b56957d0e
brunafalheiro <brunafalheiro@yahoo.com.br>
2025-02-20 23:36:48 -0300

    Add go back to lesson screen

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="flex">
     <div class="full-width">
-      <EduButton title="Voltar" :onclick="goBack" />
-      
       <div class="main-content">
         <router-view></router-view>
       </div>
@@ -16,12 +14,7 @@
 </template>
 
 <script setup>
-  import router from './router/router';
   import MenuCard from './components/MenuCard/MenuCard.vue';
-  import EduButton from './components/EduButton/EduButton.vue';
-
-  const goBack = () => { router.back(); }
-
 </script>
 
 <style lang="scss">@import 'App.scss';</style>
--- src/ui/LessonScreen/LessonScreen.vue
+++ src/ui/LessonScreen/LessonScreen.vue
@@ -1,12 +1,16 @@
 <template>
+  <EduButton title="Voltar" :onclick="goBack" />
   <div>Lesson {{routeId}}</div>
 </template>
 
 <script setup>
   import { useRoute } from 'vue-router';
+  import router from '../../router/router.js';
+  import EduButton from '../../components/EduButton/EduButton.vue';
 
   const route = useRoute();
   const routeId = route.params.id;
+  const goBack = () => { router.back(); }
 </script>
 
 <style lang="scss">@import 'LessonScreen.scss';</style>
