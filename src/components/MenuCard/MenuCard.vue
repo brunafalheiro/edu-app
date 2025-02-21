dd91f9f53b48633b4e7227a9eb74cbd81a993ee5
brunafalheiro <brunafalheiro@yahoo.com.br>
2025-02-21 18:06:13 -0300

    Add selected style to menu card

--- src/components/MenuCard/MenuCard.vue
+++ src/components/MenuCard/MenuCard.vue
@@ -1,13 +1,21 @@
 <template>
-  <div class="menu-card flex space-between pointer pt-24 pr-24 pb-24 pl-24">
+  <div :class="['menu-card', 'flex', 'space-between', 'pointer', 'pt-24', 'pr-24', 'pb-24', 'pl-24', { 'selected': isSelectedCard }]" @click="redirectTo">
     <p class="title">{{title}}</p>
     <div class="icon"></div>
   </div>
 </template>
 
 <script setup>
-  import { defineProps } from 'vue'
-  defineProps({ title: String });
+  import { defineProps, computed } from 'vue';
+  import router from '../../router/router.js';
+
+  const props = defineProps({
+    title: String,
+    routeToRedirect: String,
+  });
+
+  const isSelectedCard = computed(() => router.currentRoute.value.name === props.routeToRedirect);
+  const redirectTo = () => { router.push({ name: props.routeToRedirect }); };
 </script>
 
 <style lang="scss">@import 'MenuCard.scss';</style>
