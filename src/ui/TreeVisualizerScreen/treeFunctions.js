731a50f48425a78f8a1ad8b7906aaef0a2a0afa8
brunafalheiro <brunafalheiro@yahoo.com.br>
2025-03-13 18:22:38 -0300

    Add visiting and found flags

--- src/ui/TreeVisualizerScreen/treeFunctions.js
+++ src/ui/TreeVisualizerScreen/treeFunctions.js
@@ -3,6 +3,8 @@ class TreeNode {
 		this.value = value;
 		this.left = null;
 		this.right = null;
+		this.isBeingVisited = false;
+		this.found = false;
 	}
 }
 
@@ -12,24 +14,69 @@ class TreeFunctions {
   }
 
   static insertNode(tree, value) {
-    // Rule 1: If tree is empty, create root
     if (!tree) return new TreeNode(value);
     
-    // Rule 2: If the value is less than the root, insert in left subtree
     if (value < tree.value) {
       tree.left = TreeFunctions.insertNode(tree.left, value);
       return tree;
     }
   
-    // Rule 3: If the value is greater than the root, insert in right subtree
     if (value > tree.value) {
       tree.right = TreeFunctions.insertNode(tree.right, value);
       return tree;
     }
     
-    // If the value is equal to the root, do nothing (abort insertion)
     console.log("Value already exists in the tree");
     return tree;
+  }
+
+  static async searchNode(tree, value) {
+    if (!tree) return null;
+
+    // Reset the 'found' flag in the tree
+    TreeFunctions.resetFoundFlag(tree);
+
+    // Highlight the node being visited
+    tree.isBeingVisited = true;
+    console.log("visiting", tree.value);
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    if (tree.value === value) {
+      tree.isBeingVisited = false;
+      tree.found = true;
+      return tree;
+    }
+
+    tree.isBeingVisited = false;
+
+    if (value < tree.value) return TreeFunctions.searchNode(tree.left, value);
+    return TreeFunctions.searchNode(tree.right, value);
+  }
+
+  // Auxiliar function to reset the 'found' flag in the tree
+  static resetFoundFlag(tree) {
+    if (!tree) return;
+    tree.found = false;
+    TreeFunctions.resetFoundFlag(tree.left);
+    TreeFunctions.resetFoundFlag(tree.right);
+  }
+
+  static generateRandomTree(nodeCount) {
+    if (nodeCount <= 0) return null;
+
+    let usedValues = new Set();
+    while (usedValues.size < nodeCount) {
+      usedValues.add(Math.floor(Math.random() * 101));
+    }
+
+    let values = [...usedValues];
+    let root = new TreeNode(values[0]);
+
+    for (let i = 1; i < values.length; i++) {
+      TreeFunctions.insertNode(root, values[i]);
+    }
+
+    return root;
   }
 }
 
