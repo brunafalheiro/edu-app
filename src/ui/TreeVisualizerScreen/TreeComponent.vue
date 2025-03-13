5c3c30b1e37170941c4b1eed7d284433835938ff
brunafalheiro <brunafalheiro@yahoo.com.br>
2025-03-13 14:46:24 -0300

    Init tree component

---
+++ src/ui/TreeVisualizerScreen/TreeComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul>
+    <li>
+      <a href="#">{{ tree.value }}</a>
+      <ul v-if="tree.left || tree.right">
+        <li v-if="tree.left">
+          <TreeComponent :tree="tree.left" />
+        </li>
+        <li v-if="tree.right">
+          <TreeComponent :tree="tree.right" />
+        </li>
+      </ul>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+  import { defineProps } from 'vue';
+
+  defineProps({ tree: Object });
+</script>
+
+<style>@import 'TreeComponent.css'</style>
