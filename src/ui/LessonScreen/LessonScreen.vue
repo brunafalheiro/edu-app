15c87e4da462f371524e655d8e5b417fb4607f38
brunafalheiro <brunafalheiro@yahoo.com.br>
2025-03-13 15:57:21 -0300

    Add placeholder content in course

--- src/ui/LessonScreen/LessonScreen.vue
+++ src/ui/LessonScreen/LessonScreen.vue
@@ -1,15 +1,29 @@
 <template>
-  <EduButton title="Voltar" :onclick="goBack" />
-  <div>Lesson {{routeId}}</div>
+  <div class="full-width pt-48 pr-48 pl-48 pb-48">
+    <EduButton title="Voltar" :onclick="goBack" />
+    
+    <div v-if="lesson">
+      <h1>{{ lesson.title }}</h1>
+      <p class="mb-20">{{ lesson.description }}</p>
+      <div v-for="(item, index) in lesson.content" :key="index" class="flex full-width">
+        <p v-if="item.type === 'text'" class="mt-20">{{ item.value }}</p>
+        <img v-else-if="item.type === 'image'" :src="item.value" alt="Imagem da lição" class="mx-auto mt-20"/>
+        <video v-else-if="item.type === 'video'" :src="item.value" controls class="mt-20"></video>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
   import { useRoute } from 'vue-router';
   import router from '../../router/router.js';
   import EduButton from '../../components/EduButton/EduButton.vue';
+  import lessons from '@/lessons.json';
+  import { ref } from 'vue';
 
   const route = useRoute();
-  const routeId = route.params.id;
+  const lessonId = route.params.id;
+  const lesson = ref(lessons.find(lesson => lesson.id === lessonId));
   const goBack = () => { router.back(); }
 </script>
 
